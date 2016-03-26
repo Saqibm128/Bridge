c2ad91827bd1b7df8cbe94c53dfa3880870b8056
ArielBeecham <ariel.beecham@gmail.com>
2016-03-26 03:55:11 -0400

    added even more stuff pt. 2

--- app/src/main/java/hackgsu/tgd/restaurantsforchronicillness/MapOrListActivity.java
+++ app/src/main/java/hackgsu/tgd/restaurantsforchronicillness/MapOrListActivity.java
@@ -1,5 +1,6 @@
 package hackgsu.tgd.restaurantsforchronicillness;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,11 +8,11 @@ import android.view.View;
 public class MapOrListActivity extends AppCompatActivity {
 
     public void onClickMap(View view) {
-
+        Intent intent = new Intent(getApplicationContext(), MapActivity.class);
     }
 
     public void onClickList(View view) {
-
+        Intent intent = new Intent(getApplicationContext(), ListActivity.class);
     }
 
     @Override
