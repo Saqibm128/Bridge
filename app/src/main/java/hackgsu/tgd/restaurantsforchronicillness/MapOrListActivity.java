d32f61bc78d9d0e556c3b0be3100ccab88bbc6bd
ArielBeecham <ariel.beecham@gmail.com>
2016-03-26 03:19:03 -0400

    added more stuff

---
+++ app/src/main/java/hackgsu/tgd/restaurantsforchronicillness/MapOrListActivity.java
@@ -0,0 +1,22 @@
+package hackgsu.tgd.restaurantsforchronicillness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MapOrListActivity extends AppCompatActivity {
+
+    public void onClickMap(View view) {
+
+    }
+
+    public void onClickList(View view) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_or_list);
+    }
+}
